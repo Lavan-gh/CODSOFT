e8f006c0f6cc02fd11bdaaa88d7c8d3be2669de0
Lavan-gh <lavantrial@gmail.com>
2026-01-03 11:18:47 +0530

    Update script.js

--- web-development/landing-page/script.js
+++ web-development/landing-page/script.js
@@ -4,7 +4,6 @@ scrollBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
-// Learn More button scroll to Features section
 const learnBtn = document.getElementById("learn-more");
 learnBtn.addEventListener("click", () => {
   document.getElementById("features").scrollIntoView({ behavior: 'smooth' });
